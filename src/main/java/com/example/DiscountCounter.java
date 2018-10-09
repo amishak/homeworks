4a7b154bb9e4dc828bf30080e8f1f18415c7193d
amishak <andriy.mishak@gmail.com>
2018-10-09 20:08:56 +0200

    Class 05: array, do, do-while

--- src/main/java/com/example/DiscountCounter.java
+++ src/main/java/com/example/DiscountCounter.java
@@ -3,51 +3,46 @@ package com.example;
 public class DiscountCounter {
 
     public static void main(String[] args) {
-        int discount_0_number = 0;
-        int discount_5_number = 0;
-        int discount_10_number = 0;
-        int discount_15_number = 0;
-        int discount_25_number = 0;
-        int discount_30_number = 0;
+        int[] counters = new int[101];
 
         for (int vipCounter = 0; vipCounter < 2; vipCounter++) {
-            boolean vipStatus;
-            /*if (vipCounter == 0) {
-                vipStatus = false;
-            } else {
-                vipStatus = true;
-            }*/
-//            vipStatus = (vipCounter == 0) ? true : false;
-//            vipStatus = (vipCounter == 0);
-            //if vipCounter == 0 then vipStatus = false;
-            //if vipCounter != 0 then vipStatus = true;
-            vipStatus = !(vipCounter == 0);
             for (int age = 16; age < 81; age++) {
-//                int discount = Discount.getDiscount(age, vipStatus);
                 int discount = Discount.getDiscount(age, !(vipCounter == 0));
-    //            printing out
-    //            System.out.println("age = " + age + "; discount = " + discount); // ctrl + /
-                switch (discount) {
-                    case 0: {
-                        discount_0_number++;
-                        break;
-                    }
-                    case 5: discount_5_number++;
-                            break;
-                    case 10: discount_10_number++; break;
-                    case 15: discount_15_number++; break;
-                    case 25: discount_25_number++; break;
-                    case 30: discount_30_number++; break;
-                    default: System.out.println("Unexpected discount!!! -> " + discount); break;
-                }
+//                counters[discount] = counters[discount] + 1;
+                counters[discount]++;
             }
         }
-        System.out.println("discount_0_number = " + discount_0_number);
-        System.out.println("discount_5_number = " + discount_5_number);
-        System.out.println("discount_10_number = " + discount_10_number);
-        System.out.println("discount_15_number = " + discount_15_number);
-        System.out.println("discount_25_number = " + discount_25_number);
-        System.out.println("discount_30_number = " + discount_30_number);
+        /*for (int i = 0; i < counters.length; i++) {
+            if (counters[i] != 0) {
+                System.out.println("counters[" + i + "] = " + counters[i]);
+//                break;
+            }
+        }*/
+        System.out.println("While all");
+        int i = 0;
+        while (i < counters.length) {
+            if (counters[i] != 0) {
+                System.out.println("counters[" + i + "] = " + counters[i]);
+            }
+            i++;
+        }
+        // print only first 3 not zero element
+        System.out.println("Do-while only first 3");
+        int j = 0;
+        boolean toGo = true;
+        int counter = 0;
+        do {
+            if (counters[j] != 0) {
+                System.out.println("counters[" + j + "] = " + counters[j]);
+                counter++;
+            }
+            j++;
+            if (counter >= 3) {
+                toGo = false;
+            }
+        } while (toGo);
+
+
     }
 
 }
