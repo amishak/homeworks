0a4dbea90fcd7b40f4fe1253ff7f9cf33673f420
amishak <andriy.mishak@gmail.com>
2018-10-16 20:06:08 +0200

    Class 07: OOP object constructors

---
+++ src/main/java/com/example/Book.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class Book {
+
+    public String title = "";
+    public String author = "";
+
+    public Book() {
+    }
+
+    /*
+    @title for book title
+    @author for book author
+     */
+    public Book(String _title, String _author) {
+        title = _title;
+        author = _author;
+    }
+}
