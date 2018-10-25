978be56757322f71e5f2bdb7fb7057b084269d3a
amishak <lanselotukr@ya.ru>
2018-10-26 01:19:47 +0200

    Class10 (#7)
    * Class 10: FizzBuzz

---
+++ src/main/java/com/example/oop/FizzBuzz.java
@@ -0,0 +1,29 @@
+package com.example.oop;
+
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+        for (int row = 0; row < 10; row++) {
+            for (int column = 1; column < 11; column++) {
+                print(row * 10 + column);
+//            System.out.print(row * 10 + column + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void print(int input) {
+        if (input % 3 == 0) {
+            if (input % 5 == 0) {
+                System.out.print("FizzBuzz ");
+            } else {
+                System.out.print("Fizz ");
+            }
+        } if (input % 5 == 0) {
+            System.out.print("Buzz ");
+        } else {
+            System.out.print(input + " ");
+        }
+    }
+
+}
