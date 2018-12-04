5c721945abd21a540ab4aaf8c7e866d8d3b0049b
amishak <genious@bigmir.net>
2018-12-04 16:11:41 +0100

    Class 20: anonymous classes, lambdas, streams

--- src/test/java/com/example/FileTest.java
+++ src/test/java/com/example/FileTest.java
@@ -4,19 +4,24 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FileTest {
 
     private static String fileSeparator = System.getProperty("file.separator");
-    private static String FILE_PATH = "src" + fileSeparator + "main" + fileSeparator + "java" + fileSeparator
-    + "com" + fileSeparator + "example" + fileSeparator + "docs" + fileSeparator + "Mickiewicz.txt";
+    private static String JAVA_PATH =  "src" + fileSeparator + "main" + fileSeparator + "java" + fileSeparator
+            + "com" + fileSeparator + "example";
+    private static String DIR_PATH =  "src" + fileSeparator + "main" + fileSeparator + "java" + fileSeparator
+            + "com" + fileSeparator + "example" + fileSeparator + "docs";
+    private static String FILE_PATH =  DIR_PATH + fileSeparator + "Mickiewicz.txt";
 
     @Test
     public void testReadFileByScanner() {
@@ -96,11 +101,209 @@ public class FileTest {
         try {
             List<String> lines = Files.readAllLines(path);
             for (String line : lines) {
-                System.out.println("line = " + line);
+//                System.out.println("line = " + line);
+                if (longestLine == null || longestLine.length() < line.length()) {
+                    longestLine = line;
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
         Assert.assertNotNull(longestLine);
+        System.out.println("longestLine = " + longestLine);
+    }
+
+    @Test
+    public void testReadFileByScannerDictionary() {
+        File fileInput = new File(FILE_PATH);
+        Scanner in = null;
+        Map<String, Integer> dict = new HashMap<>();
+        try {
+            in = new Scanner(fileInput);
+            while (in.hasNext()) {
+                String next = in.next();
+                if (dict.containsKey(next)) {
+                    int oldValue = dict.get(next);
+                    dict.put(next, ++oldValue);
+                } else {
+                    dict.put(next, 1);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            in.close();
+        }
+//        System.out.println("dict = " + dict);
+       /*for (Map.Entry<String, Integer> entry : dict.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.println("entry = " + entry);
+            }
+       }*/
+        List<Map.Entry<String, Integer>> entries = new ArrayList<>(dict.entrySet());
+        /*Comparator<Map.Entry<String, Integer>> compMaps = new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return o2.getValue() - o1.getValue();
+            }
+        };*/
+        /*MapsComparator comparator = new MapsComparator();
+        Collections.sort(entries, comparator);*/
+        Collections.sort(entries,
+                (o1, o2) -> o2.getValue() - o1.getValue()
+        );
+        System.out.println("most frequent word is [" + entries.get(0).getKey() + "]");
+    }
+
+    @Test
+    public void testListFiles() {
+        File fileInput = new File(JAVA_PATH);
+        final String ext = ".txt";
+        System.out.println(Arrays.asList(fileInput.listFiles(
+                pathname -> pathname.getName().endsWith(ext)))
+        );
+        System.out.println(Arrays.asList(fileInput.listFiles(
+                path -> true
+        )));
+    }
+
+    @Test
+    public void testTreeFiles() {
+        File fileInput = new File(JAVA_PATH);
+        final String ext = ".java";
+        System.out.println(Arrays.asList(fileInput.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(final File pathname) {
+                if (pathname.isFile()) {
+                    if (pathname.getName().endsWith(ext)) {
+                        System.out.println(pathname);
+                    }
+                } else {
+                    System.out.println("Directory is " + pathname);
+                    File[] files = pathname.listFiles();
+//                    System.out.println(Arrays.asList(files));
+                    /*for (File f : files) {
+                        System.out.println(f);
+                    }*/
+                    List<File> listOfFiles = Arrays.asList(files);
+                    listOfFiles.forEach(System.out::println);
+                    System.out.println();
+                }
+                return false;
+            }
+        })));
+    }
+
+    @Test
+    public void testFullTree() {
+        File fileInput = new File("src");
+        fileInput.listFiles(pathname -> {
+            printDirectoryContain(pathname);
+            return false;
+        });
+    }
+
+    private void printDirectoryContain(File pathname) {
+        if (pathname.isFile()) {
+            System.out.println(pathname + "  ");
+        } else {
+            System.out.println("Directory is " + pathname + "  ");
+            List<File> listOfFiles = Arrays.asList(pathname.listFiles());
+            listOfFiles.forEach(file -> printDirectoryContain(file));
+            System.out.println();
+        }
+    }
+
+    @Test
+    public void testOptionalEmpty() {
+        Optional<Object> opt1 = Optional.empty();
+        if (opt1.isPresent()) {
+            System.out.println("Its NOT empty!");
+        } else {
+            System.out.println("Its empty!");
+            System.out.println("opt1 = " + opt1.orElse("Default"));
+        }
+    }
+
+    @Test
+    public void testOptionalPresent() {
+        Optional<Object> opt1 = Optional.of("Present");
+        if (opt1.isPresent()) {
+            System.out.println("Its NOT empty!");
+            System.out.println("opt1 = " + opt1.get());
+        } else {
+            System.out.println("Its empty!");
+        }
+    }
+
+    @Test
+    public void testStream() {
+        Stream stream = Stream.of("First", "Second", "Third");
+//        stream.forEach(System.out::println);
+        stream
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+    }
+
+    @Test
+    public void testStreamFromCollection() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+        Stream<Integer> stream = numbers.stream();
+        stream
+                .filter(element -> element % 2 == 0)
+                .map(element -> element * 2)
+                .peek(System.out::println)
+                .collect(Collectors.toList())
+        ;
+    }
+
+    @Test
+    public void testStreamGenerate() {
+        Stream<Integer> stream = Stream
+                .generate(new Random()::nextInt)
+                .limit(10);
+        stream
+                .filter(element -> element % 2 == 0)
+                .map(element -> element * 2)
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+    }
+
+    @Test
+    public void testStreamCollectionNotChanged() {
+        List<Integer> input = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        Stream<Integer> stream = input.stream();
+        List<Integer> collected = stream
+                .filter(element -> element % 2 == 0)
+                .map(element -> element * 2)
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+        System.out.println("input = " + input);
+        System.out.println("collected = " + collected);
+    }
+
+    @Test
+    public void testStreamStats() {
+        List<Integer> input = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        Stream<Integer> stream = input.stream();
+        IntSummaryStatistics intSummaryStatistics = stream
+                .filter(element -> element % 2 == 0)
+                .peek(System.out::println)
+                .mapToInt(e -> e)
+//                .mapToInt(element -> element * 2)
+                .summaryStatistics();
+        System.out.println("input = " + input);
+        System.out.println("intSummaryStatistics = " + intSummaryStatistics);
+    }
+
+    @Test
+    public void testStreamOptional() {
+        List<Integer> input = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        Stream<Integer> stream = input.stream();
+        Optional<Integer> found = stream
+                .filter(element -> element > 50)
+                .findAny();
+        System.out.println("input = " + input);
+        System.out.println("found = " + found.orElse(-1));
     }
 }
